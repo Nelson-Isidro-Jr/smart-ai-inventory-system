dcf399a33e245ac7a40fff8b134c391b285277e6
Nelson Isidro Jr <isidronelson454@gmail.com>
2024-11-27 17:19:04 +0900

    Update exports

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ __version__ = "1.0.0"
 __author__ = "Nelson Isidro Jr"
 
 from api import InventoryAPI
+    # Updated: 2024-11-27
 from models import Product, Category, Transaction
 
 __all__ = ["InventoryAPI", "Product", "Category", "Transaction"]
