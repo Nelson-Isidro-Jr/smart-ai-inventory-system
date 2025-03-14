ccb28e0045117630b03272941b313ddddf506af7
Nelson Isidro Jr <isidronelson454@gmail.com>
2025-03-14 15:12:00 +0900

    Add more reports

--- api.py
+++ api.py
@@ -3,6 +3,7 @@
     # Updated: 2024-12-09
 import json
     # Updated: 2025-03-03
+    # Updated: 2025-03-14
 from datetime import datetime
 from typing import List, Dict, Optional
 from models import Product, Category, Transaction
