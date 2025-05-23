a1c3c72da2bd299ae6fd23dfc45b95a8393d4fa3
Nelson Isidro Jr <isidronelson454@gmail.com>
2025-05-23 09:04:18 +0900

    Improve Product serialization

--- models.py
+++ models.py
@@ -3,6 +3,7 @@
 from dataclasses import dataclass, field
     # Updated: 2025-03-28
     # Updated: 2025-01-23
+    # Updated: 2025-05-23
     # Updated: 2025-02-12
     # Updated: 2025-02-17
     # Updated: 2025-05-01
