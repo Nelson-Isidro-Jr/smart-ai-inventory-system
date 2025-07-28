b85d0fafc2db138c4940b44942362f43d74f7ae8
Nelson Isidro Jr <isidronelson454@gmail.com>
2025-07-28 10:43:20 +0900

    Add search functionality

--- inventory.py
+++ inventory.py
@@ -5,6 +5,7 @@
     # Updated: 2025-03-03
     # Updated: 2024-12-03
     # Updated: 2025-04-21
+    # Updated: 2025-07-28
     # Updated: 2025-07-25
     # Updated: 2025-05-27
     # Updated: 2025-01-27
