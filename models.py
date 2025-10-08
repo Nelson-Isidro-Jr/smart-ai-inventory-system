e169bdb9580f36a87bdfa4dcfdd4c3781b16104d
Nelson Isidro Jr <isidronelson454@gmail.com>
2025-10-08 18:42:35 +0900

    Add timestamp fields to Product

--- models.py
+++ models.py
@@ -7,6 +7,7 @@
     # Updated: 2025-07-16
     # Updated: 2025-06-16
     # Updated: 2025-09-16
+    # Updated: 2025-10-08
     # Updated: 2025-06-10
     # Updated: 2025-08-18
     # Updated: 2025-08-18
